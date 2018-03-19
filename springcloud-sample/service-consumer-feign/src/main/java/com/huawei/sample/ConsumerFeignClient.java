49d6dd2ae1c2ae315093c90d0a29b5069cecca60
lijinyong <83488245@qq.com>
2018-03-19 20:54:18 +0800

    Update ConsumerFeignClient.java

--- springcloud-sample/service-consumer-feign/src/main/java/com/huawei/sample/ConsumerFeignClient.java
+++ springcloud-sample/service-consumer-feign/src/main/java/com/huawei/sample/ConsumerFeignClient.java
@@ -4,9 +4,12 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("cse-provider")
+@FeignClient("service-provider")
 public interface ConsumerFeignClient {
 
+        @GetMapping("/hello/{name}")
+	public String hello(@PathVariable String name);
+	
 	@GetMapping("/services")
 	String services();
 
