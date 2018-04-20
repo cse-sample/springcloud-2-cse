5adf13270a9185a5d214cfaef473773e14b99bd9
qinling <yi5637@sina.com>
2018-04-20 11:17:27 +0800

    init service-consumer-ribbon-hystrix

---
+++ springcloud-2-cse-sample/service-consumer-ribbon-hystrix/src/main/java/com/huawei/sample/ConsumerApplication.java
@@ -0,0 +1,34 @@
+package com.huawei.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@ImportResource(locations = "classpath*:META-INF/spring/*.bean.xml")
+public class ConsumerApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Bean
+	@LoadBalanced
+	public AsyncRestTemplate asyncRestTemplate() {
+		return new AsyncRestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+}
