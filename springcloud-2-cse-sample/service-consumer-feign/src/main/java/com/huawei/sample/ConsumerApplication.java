a8f453215c0561be6403bd66d2deeaeab3040207
qinling <yi5637@sina.com>
2018-05-15 11:04:03 +0800

    update SDK 2.3.19

--- springcloud-2-cse-sample/service-consumer-feign/src/main/java/com/huawei/sample/ConsumerApplication.java
+++ springcloud-2-cse-sample/service-consumer-feign/src/main/java/com/huawei/sample/ConsumerApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@ImportResource(locations = "classpath*:META-INF/spring/*.bean.xml")
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
